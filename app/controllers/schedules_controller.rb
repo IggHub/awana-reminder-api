05fb27183c3d2b425a983b0d9d78f68af5c7b8d9
Iggy <igoririanto@rocketmail.com>
2017-05-03 18:02:03 -0700

    add create and destroy for schedule

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -8,4 +8,32 @@ class SchedulesController < ApplicationController
     @schedule = Schedule.find(params[:id])
     render json: @schedule
   end
+
+  def create
+    @schedule = Schedule.new(schedule_params)
+    if @schedule.save
+      render json: @schedule
+    else
+      render json: @schedule, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    @schedule = Schedule.find(params[:id])
+    if @schedule.update_attributes
+      render json: @schedule
+    else
+      render json: @schedule, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @schedule = Schedule.find(params[:id]).destroy
+    head :no_content
+  end
+
+  private
+  def schedule_params
+    params.permit(:date, :user_id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   scope :api do
     resources :workers, only: [:index, :show]
     resources :users, only: [:index, :show]
-    resources :schedules, only: [:index, :show]
+    resources :schedules, only: [:index, :show, :create, :update, :destroy]
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
