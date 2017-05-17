d27d140f50d9d19140faab3e3b7b95f683df2585
Iggy <igoririanto@rocketmail.com>
2017-05-17 14:37:44 -0700

    add worker length validation

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,5 +1,6 @@
 class Schedule < ApplicationRecord
   belongs_to :user
-  has_many :workers, dependent: :destroy
   has_many :texts, dependent: :destroy
+  has_many :workers, dependent: :destroy
+  validates_length_of :workers, maximum: 3
 end
--- app/models/worker.rb
+++ app/models/worker.rb
@@ -1,4 +1,5 @@
 class Worker < ApplicationRecord
   belongs_to :schedule, optional: true
+  validates_associated :schedule
   scope :schedule_id, -> (schedule_id) {where schedule_id: schedule_id}
 end
