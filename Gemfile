420e97c655698344581d42eaa3b50626b8676366
Iggy <igoririanto@rocketmail.com>
2017-06-21 15:00:01 -0700

    add devise and start editing sign_in, sign_up, and edit

--- Gemfile
+++ Gemfile
@@ -18,6 +18,7 @@ gem 'foreman'
 gem 'twilio-ruby', '~> 4.11.1'
 gem 'pg'
 gem 'faker', '~> 1.7', '>= 1.7.3'
+gem 'devise'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,14 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
+  root 'welcome#index'
 
-  root "app#index"
   get 'app', to: 'app#index'
+  post '/text_it' => 'schedules#text_dat_message'
+  devise_for :users
   scope :api do
     resources :workers, only: [:index, :show, :create, :update, :destroy]
     resources :users, only: [:index, :show]
     resources :schedules, only: [:index, :show, :create, :update, :destroy]
     resources :texts, only: [:index, :create]
     resources :rosters, only: [:index, :create]
-    #resources :sms, only: :create
   end
-  post '/text_it' => 'schedules#text_dat_message'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
