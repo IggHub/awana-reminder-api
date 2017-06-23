f1e351a76f19d8ff92602d73299c56e195adb998
Iggy <igoririanto@rocketmail.com>
2017-06-23 11:41:28 -0700

    add links and edit fonts

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
   root 'welcome#index'
+  
+  get 'welcome/contact'
+  get 'welcome/help'
 
   get 'app', to: 'app#index'
+
   post '/text_it' => 'schedules#text_dat_message'
+
   devise_for :users
+
   scope :api do
     resources :workers, only: [:index, :show, :create, :update, :destroy]
     resources :users, only: [:index, :show]
